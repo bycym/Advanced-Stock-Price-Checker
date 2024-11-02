20986d8a2853708aa7af3c92c24ad5ee61313da3
Aron <aronbenkoczy@kinsta.com>
2024-11-02 23:10:45 +0100

    feat: add logger

---
+++ src/lib/logger/logger.ts
@@ -0,0 +1,22 @@
+import { format, transports, createLogger } from "winston";
+
+export const logger = createLogger({
+  level: "info",
+  format: format.combine(
+    format.timestamp(),
+    format.colorize(),
+    format.simple(),
+    format.printf(
+      (info) =>
+        `[${info.level}] ${info.timestamp}\t:: ${info.message}` +
+        (info.splat !== undefined ? `${info.splat}` : " ")
+    )
+  ),
+  transports: [
+    new transports.Console({
+      level: "debug",
+    }),
+    new transports.File({ filename: "error.log", level: "error" }),
+  ],
+  exitOnError: false,
+});
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import { logger } from "./lib/logger/logger";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  logger.info(`Backend service Listening on Port ${process.env.PORT ?? 3000}`);
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
